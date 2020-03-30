d8988d3648783dc7c273841350760f4d29776f46
R-Mishra <52457076+R-Mishra@users.noreply.github.com>
2020-03-29 23:12:58 -0400

    Update Tableau_Visualization.m

--- Tableau_Visualization.m
+++ Tableau_Visualization.m
@@ -1,1 +1,1 @@
-<center><iframe src="https://public.tableau.com/profile/rohit.mishra8325#!/vizhome/AnalysisofUSWildfireData/Analysis_Of_US_WIldfires?=yes" width="1004" height="1269" frameborder="0"></iframe></center>
+<center><iframe src="https://public.tableau.com/views/AnalysisofUSWildfireData/Analysis_Of_US_WIldfires?:display_count=y&publish=yes&:origin=viz_share_link" width="1004" height="1269" frameborder="0"></iframe></center>
